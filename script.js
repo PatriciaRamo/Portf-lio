0e6c1502e7a4716d81f3a0e8f8113ecbc6553dc2
Patricia14 <140025849+PatriciaRamo@users.noreply.github.com>
2023-10-04 21:53:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function typeWriter( elemento){
+    const textoArray=elemento.innerHTML.split('');
+   elemento.innerHTML='';
+    textoArray.forEach((letra,i)=>{
+setTimeout(function(){
+elemento.innerHTML+= letra
+},80*i)
+    })
+}
+const titulo= document.querySelector('span');
+typeWriter(titulo);   
+
+
